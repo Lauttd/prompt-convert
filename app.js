cbe8bf91f8285f0190515f6ae30c4e439bdfe231
Lauttd <lautarayala123090@gmail.com>
2025-05-12 00:54:37 -0300

    Terminado

--- app.js
+++ app.js
@@ -9,5 +9,18 @@ function conversion(valorArs){
 
 do{
     eleccionUsuario = parseInt(prompt("Bienvenido! \n 1)Divisa (ARS --> USD) \n 2)Salir"));
-}while
+    switch (eleccionUsuario){
+        case 1: 
+        valorArs = parseInt(prompt("ingrese la cantidad de pesos que desea cambiar a USD"));
+        conversion(valorArs);
+        alert("valor: " + conversion(valorArs).toFixed(2));
+        historial.push({
+            tipo: "divisa:",
+            entrada: valorArs + "ARS",
+            salida: conversion(valorArs) + "USD",
+            fecha: new Date().toLocaleDateString(2)});
+    }
+}while (eleccionUsuario != 2){
+    console.log(historial);
+}
 
