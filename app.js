16f7f73a48a67b2f426c5827dd7c90c8bc145159
Lauttd <lautarayala123090@gmail.com>
2025-05-12 00:38:03 -0300

    inicio del bucle

---
+++ app.js
@@ -0,0 +1,13 @@
+let eleccionUsuario;
+let valorArs;
+let valorusd = 1300;
+historial = [];
+
+function conversion(valorArs){
+    return valorArs / valorusd;
+}
+
+do{
+    eleccionUsuario = parseInt(prompt("Bienvenido! \n 1)Divisa (ARS --> USD) \n 2)Salir"));
+}while
+
